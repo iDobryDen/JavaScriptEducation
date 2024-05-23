67d07c4238d0a7471733bdfbce4a92a9a3023fa3
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-05-23 19:37:52 +0400

    calculator is work!

--- JS_Practice_5/script.js
+++ JS_Practice_5/script.js
@@ -1,126 +1,25 @@
-const p1 ={
-    name: 'Tom',
-    money: 4000,
+const firstInput = document.querySelector('.calculator-first-input');
+const secondInput = document.querySelector('.calculator-second-input');
+const plusButton = document.querySelector('.calculator-plus');
+const minusButton = document.querySelector('.calculator-minus');
+const resultBlock = document.querySelector('.calculator-result');
+
+console.log(firstInput);
+console.log(secondInput);
+console.log(plusButton);
+console.log(minusButton);
+
+plusButton.addEventListener('click', function() {
+    const firstInputValue = Number(firstInput.value);
+    const secondInputValue = Number(secondInput.value);
+    const result = firstInputValue + secondInputValue;
+    resultBlock.innerHTML = result;
 }
-
-const p2 ={
-    name: 'Peter',
-    money: 3000,
+)
+minusButton.addEventListener('click', function() {
+    const firstInputValue = Number(firstInput.value);
+    const secondInputValue = Number(secondInput.value);
+    const result = firstInputValue - secondInputValue;
+    resultBlock.innerHTML = result;
 }
-
-const p3 ={
-    name: 'Steve',
-    money: 5555,
-}
-
-const bank = p1.money + p2.money + p3.money;
-
-console.log(bank);
-
-const arr = [
-    {
-        name: 'Tom',
-        money: 4000,
-    },
-    {
-        name: 'Peter',
-        money: 6000,
-    },
-    {
-        name: 'Steve',
-        money: 7000,
-    }
-]
-
-let sum = 0;
-
-for (let i = 0; i < arr.length; i++)
-    {
-        sum = sum + arr[i].money;
-    }
-
-console.log(sum);
-
-let person = {
-    name: 'Gregory',
-    tinkoff: 10000,
-    sber: 12000,
-    vtb: 5555,
-    alfa: 6666,
-    psb: 8002
-}
-
-let result = 0;
-
-// for (prop in person) {
-//     if (prop !== 'name')
-//     result = result + person[prop];
-// }
-
-for (prop in person) {
-    if (typeof person[prop] === 'number'){
-        result = result + person[prop];
-    }
-}
-
-console.log(result);
-
-const pers = {
-    name: 'Pete',
-    banks: {
-        tinkoff: 10000,
-        sber: 12000,
-        vtb: 5000,
-        alfa: 6000,
-        psb: 8000
-    },
-    age: 30
-}
-
-let result1 = 0;
-
-for (prop in pers.banks){
-    result1 += pers.banks[prop];
-}
-
-console.log(result1);
-
-const str = 'aaabbbcccabc';
-let sum_b = 0;
-for (char of str) {
-    if (char === 'b'){
-        sum_b++;
-    }
-}
-
-console.log(sum_b);
-
-const names = [
-    'Masha',
-    'Peter',
-    'Olya',
-    'Dina',
-    'Sergei',
-    'Donovan',
-    'Solomon',
-    'Noah',
-    'Dodik'
-]
-let name_with_d = 0;
-// for (let i = 0; i < names.length; i++) {
-//     for (char of names[i]){
-//         if (char === 'D'){
-//             name_with_d++;
-//         }
-//     }
-// }
-
-for (let i = 0; i < names.length; i++)
-    {
-        if (names[i][0] === 'D' || names[i][0] === 'd'){
-            name_with_d++;
-        }
-    }
-
-
-console.log(name_with_d);
+)
