3f244b389c6f6035fc36a5ebc98714bca8a96759
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-05-23 20:13:06 +0400

    culculate done

--- JS_Practice_5/script.js
+++ JS_Practice_5/script.js
@@ -2,6 +2,8 @@ const firstInput = document.querySelector('.calculator-first-input');
 const secondInput = document.querySelector('.calculator-second-input');
 const plusButton = document.querySelector('.calculator-plus');
 const minusButton = document.querySelector('.calculator-minus');
+const multiplyButton = document.querySelector('.calculator-multiply');
+const divideButton = document.querySelector('.calculator-divide');
 const resultBlock = document.querySelector('.calculator-result');
 
 console.log(firstInput);
@@ -9,17 +11,30 @@ console.log(secondInput);
 console.log(plusButton);
 console.log(minusButton);
 
-plusButton.addEventListener('click', function() {
+function sum(a, b){
+    return a + b;
+}
+
+function minus(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operation(func) {
     const firstInputValue = Number(firstInput.value);
     const secondInputValue = Number(secondInput.value);
-    const result = firstInputValue + secondInputValue;
+    let result = func(firstInputValue, secondInputValue);
     resultBlock.innerHTML = result;
 }
-)
-minusButton.addEventListener('click', function() {
-    const firstInputValue = Number(firstInput.value);
-    const secondInputValue = Number(secondInput.value);
-    const result = firstInputValue - secondInputValue;
-    resultBlock.innerHTML = result;
-}
-)
+
+plusButton.addEventListener('click', () => operation(sum));
+minusButton.addEventListener('click', () => operation(minus));
+multiplyButton.addEventListener('click', () => operation(multiply));
+divideButton.addEventListener('click', () => operation(divide));
